3f4ac01c0a7740962f41586d9cbba35506ee0168
My Dell <My Dell@192.168.100.5>
2021-12-07 22:36:19 +0530

    Added

--- learning/src/com/wrapperclasses/learning/WrapperClass.java
+++ learning/src/com/wrapperclasses/learning/WrapperClass.java
@@ -1,11 +1,32 @@
 package com.wrapperclasses.learning;
 
-public class WrapperClass {
+import java.io.File;
+import java.io.FileInputStream;
 
+public class WrapperClass {
+	public void callMe(){
+		
+		System.out.println("In the Loop");
+		callMe();
+	}
 	public static void main(String[] args) {
-		    int a = 8;
+		WrapperClass obj= new WrapperClass();
+		for(;;){
+			obj.callMe();
+		}
+	}
+}
+		
+		 /*   int a = 8;
+		    int iAge = 12;
 			    double b = 5.65;
+			    float c =5.2f;
 			  Integer aObj =a;
+			  Integer d=null;
+			  File f=new File("");
+			//  FileInputStream fis = new FileInputStream(new File(""));
+			  System.out.println(d.intValue());
+			  
 			    Double bObj = Double.valueOf(b);
 
 			    if(aObj instanceof Integer) {
@@ -17,3 +38,4 @@ public class WrapperClass {
 			    }
 			  }
 			}
+*/
